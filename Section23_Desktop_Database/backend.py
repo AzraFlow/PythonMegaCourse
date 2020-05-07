0224a0639dfe5e3679fe23972b57b185cf377706
mbuhidar <mbuhidar@gmail.com>
2020-05-06 23:29:55 -0400

    Add Section22 and 23 files.

---
+++ Section23_Desktop_Database/backend.py
@@ -0,0 +1,34 @@
+#! python
+
+import sqlite3
+
+
+def connect():
+    conn = sqlite3.connect("books.db")
+    cur = conn.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS book (id INTEGER PRIMARY KEY, \
+                title TEXT, author TEXT, year INTEGER, isbn INTEGER)")
+    conn.commit()
+    conn.close()
+
+
+def insert(title, author, year, isbn):
+    conn = sqlite3.connect("books.db")
+    cur = conn.cursor()
+    cur.execute("INSERT INTO book VALUES (NULL, ?, ?, ?, ?)", (title, author, year, isbn))
+    conn.commit()
+    conn.close()
+
+
+def view():
+    conn = sqlite3.connect("books.db")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM book")
+    rows = cur.fetchall()
+    conn.close()
+    return rows
+
+
+connect()
+insert("The Sea", "John Tablet", 1918, 913123132)
+print(view())
